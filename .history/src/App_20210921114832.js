a39b0204cf43f629e6a16e2f2cd7b76d362c33fa
tractortoby <tobychung1993@gmail.com>
2021-09-21 11:52:59 +0800

    初始化项目

---
+++ .history/src/App_20210921114832.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">项目根目录</div>
+  );
+}
+
+export default App;
---
+++ .history/src/App_20210921114939.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">项目根目录</div>
+  );
+}
+
+export default App;
