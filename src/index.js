f085df1c72bd8ae4d4e39714f05aca71684c25c7
tractortoby <tobychung1993@gmail.com>
2021-09-21 19:08:30 +0800

    修改TabBar外观样式

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-//导入antd-mobile的样式
+// 导入antd-mobile的样式
 import 'antd-mobile/dist/antd-mobile.css';
+
+// 导入字体图标库的样式文件
+import './assets/fonts/iconfont.css'
 
 import './index.css';
 
