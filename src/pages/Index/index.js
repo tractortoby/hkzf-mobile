41300e2e1a4de366229fc649f45f8ecd1318ad8c
tractortoby <tobychung1993@gmail.com>
2021-09-23 15:54:24 +0800

    修复轮播图切换错误

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,91 +1,116 @@
 import React from "react";
-import { Carousel, Flex } from 'antd-mobile';
+import { Carousel, Flex } from "antd-mobile";
 import axios from "axios";
 
 // 导入导航菜单图片
-import Nav1 from "../../assets/images/nav-1.png"
-import Nav2 from "../../assets/images/nav-2.png"
-import Nav3 from "../../assets/images/nav-3.png"
-import Nav4 from "../../assets/images/nav-4.png"
+import Nav1 from "../../assets/images/nav-1.png";
+import Nav2 from "../../assets/images/nav-2.png";
+import Nav3 from "../../assets/images/nav-3.png";
+import Nav4 from "../../assets/images/nav-4.png";
 
-import './index.css'
+import "./index.css";
 
 // 导航菜单数据
-
+const navs = [
+  {
+    id: 1,
+    img: Nav1,
+    title: "整租",
+    path: "/home/list",
+  },
+  {
+    id: 2,
+    img: Nav2,
+    title: "合租",
+    path: "/home/list",
+  },
+  {
+    id: 3,
+    img: Nav3,
+    title: "地图找房",
+    path: "/map",
+  },
+  {
+    id: 4,
+    img: Nav4,
+    title: "去出租",
+    path: "/rent",
+  },
+];
 
 export default class Index extends React.Component {
   state = {
     // 轮播图状态数据
-    swipers: []
-  }
+    swipers: [],
+    isSwipersLoaded: false,
+  };
 
   // 获取轮播图数据的方法
   async getSwipers() {
-    const res = await axios.get('http://localhost:8080/home/swiper')
+    const res = await axios.get("http://localhost:8080/home/swiper");
     this.setState(() => {
       return {
-        swipers: res.data.body
-      }
-    })
+        swipers: res.data.body,
+        isSwipersLoaded: true,
+      };
+    });
   }
 
   componentDidMount() {
-    this.getSwipers()
+    this.getSwipers();
   }
 
   // 渲染轮播图结构
   renderSwipers() {
-    return (this.state.swipers.map(item => (
+    return this.state.swipers.map((item) => (
       <a
         key={item.id}
         href="http://www.tobychung.com"
-        style={{ display: 'inline-block', width: '100%', height: 212 }}
+        style={{ display: "inline-block", width: "100%", height: 212 }}
       >
         <img
           src={`http://localhost:8080${item.imgSrc}`}
           alt=""
-          style={{ width: '100%', verticalAlign: 'top' }}
+          style={{ width: "100%", verticalAlign: "top" }}
           onLoad={() => {
             // fire window resize event to change height
-            window.dispatchEvent(new Event('resize'));
-            this.setState({ imgHeight: 'auto' });
+            window.dispatchEvent(new Event("resize"));
+            this.setState({ imgHeight: "auto" });
           }}
         />
       </a>
-    )))
+    ));
+  }
+
+  // 渲染导航菜单
+  renderNavs() {
+    return navs.map((item) => (
+      <Flex.Item
+        key={item.id}
+        onClick={() => this.props.history.push(item.path)}
+      >
+        <img src={item.img} alt="" />
+        <h2>{item.title}</h2>
+      </Flex.Item>
+    ));
   }
 
   render() {
     return (
       <div className="index">
         {/* 轮播图 */}
-        <Carousel
-          autoplay={true}
-          infinite
-          autoplayInterval={5000}
-        >
-          {this.renderSwipers()}
-        </Carousel>
+        <div className="swiper">
+          {this.state.isSwipersLoaded ? (
+            <Carousel autoplay={true} infinite autoplayInterval={5000}>
+              {this.renderSwipers()}
+            </Carousel>
+          ) : (
+            ""
+          )}
+        </div>
 
         {/* 导航菜单 */}
-        <Flex className="nav">
-          <Flex.Item>
-            <img src={Nav1} alt="" />
-            <h2>整租</h2>
-          </Flex.Item>
-          <Flex.Item>
-            <img src={Nav2} alt="" />
-            <h2>合租</h2>
-          </Flex.Item>
-          <Flex.Item>
-            <img src={Nav3} alt="" />
-            <h2>地图找房</h2>
-          </Flex.Item>
-          <Flex.Item>
-            <img src={Nav4} alt="" />
-            <h2>去出租</h2>
-          </Flex.Item>
-        </Flex>
+        <Flex className="nav">{this.renderNavs()}</Flex>
       </div>
     );
   }
