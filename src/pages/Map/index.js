9466da23a4bbf4e89f785135f86624791d6ca2f9
tractortoby <tobychung1993@gmail.com>
2021-09-24 22:46:45 +0800

    错误引入百度地图API为BMapGL版本，实际项目为API v3.0版本，现已更正

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React from 'react'
 
+// 导入样式
 import './index.scss'
 
 export default class Map extends React.Component {
   componentDidMount() {
     // 初始化地图实例
-    const map = new window.BMapGL.Map("container");
-    const point = new window.BMapGL.Point(116.404, 39.915);
-    map.centerAndZoom(point, 15); 
+    // 注意：在 react 脚手架中全局对象需要使用 window 来访问，否则，会造成 ESLint 校验错误
+    const map = new window.BMap.Map('container')
+    // 设置中心点坐标
+    const point = new window.BMap.Point(116.404, 39.915)
+    // 初始化地图
+    map.centerAndZoom(point, 15)
   }
   render() {
     return (
       <div className="map">
-        <div id="container"></div>
+        {/* 地图容器元素 */}
+        <div id="container" />
       </div>
-    );
+    )
   }
 }
