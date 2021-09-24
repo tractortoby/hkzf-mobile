dc32acc67fdc1ac6cca9bc1bfcddd356d14bd7e0
tractortoby <tobychung1993@gmail.com>
2021-09-24 16:49:45 +0800

    替换之前写的css

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,114 +1,110 @@
-import React from "react";
-import { Carousel, Flex, Grid } from "antd-mobile";
-import axios from "axios";
+import React from 'react'
+
+// 导入组件
+import { Carousel, Flex, Grid } from 'antd-mobile'
+
+// 导入axios
+import axios from 'axios'
 
 // 导入导航菜单图片
-import Nav1 from "../../assets/images/nav-1.png";
-import Nav2 from "../../assets/images/nav-2.png";
-import Nav3 from "../../assets/images/nav-3.png";
-import Nav4 from "../../assets/images/nav-4.png";
+import Nav1 from '../../assets/images/nav-1.png'
+import Nav2 from '../../assets/images/nav-2.png'
+import Nav3 from '../../assets/images/nav-3.png'
+import Nav4 from '../../assets/images/nav-4.png'
 
-import "./index.scss";
+// 导入样式文件
+import './index.scss'
 
 // 导航菜单数据
 const navs = [
   {
     id: 1,
     img: Nav1,
-    title: "整租",
-    path: "/home/list",
+    title: '整租',
+    path: '/home/list'
   },
   {
     id: 2,
     img: Nav2,
-    title: "合租",
-    path: "/home/list",
+    title: '合租',
+    path: '/home/list'
   },
   {
     id: 3,
     img: Nav3,
-    title: "地图找房",
-    path: "/map",
+    title: '地图找房',
+    path: '/map'
   },
   {
     id: 4,
     img: Nav4,
-    title: "去出租",
-    path: "/rent",
-  },
-];
-
-// 租房小组数据
-const data = Array.from(new Array(4)).map((_val, i) => ({
-  icon: "https://gw.alipayobjects.com/zos/rmsportal/nywPmnTAvTmLusPxHPSu.png",
-  text: `name${i}`,
-}));
+    title: '去出租',
+    path: '/rent'
+  }
+]
 
 export default class Index extends React.Component {
   state = {
     // 轮播图状态数据
     swipers: [],
-    isSwipersLoaded: false,
+    isSwiperLoaded: false,
 
     // 租房小组数据
-    groups: [],
-  };
+    groups: []
+  }
 
   // 获取轮播图数据的方法
   async getSwipers() {
-    const res = await axios.get("http://localhost:8080/home/swiper");
-    this.setState(() => {
-      return {
-        swipers: res.data.body,
-        isSwipersLoaded: true,
-      };
-    });
+    const res = await axios.get('http://localhost:8080/home/swiper')
+    this.setState({
+      swipers: res.data.body,
+      isSwiperLoaded: true
+    })
   }
 
   // 获取租房小组数据的方法
   async getGroups() {
-    const res = await axios.get("http://localhost:8080/home/groups", {
+    const res = await axios.get('http://localhost:8080/home/groups', {
       params: {
-        area: "AREA%7C88cff55c-aaa4-e2e0",
-      },
-    });
+        area: 'AREA%7C88cff55c-aaa4-e2e0'
+      }
+    })
 
-    // console.log(res);
+    // console.log(res)
     this.setState({
-      groups: res.data.body,
-    });
+      groups: res.data.body
+    })
   }
 
   componentDidMount() {
-    this.getSwipers();
-    this.getGroups();
+    this.getSwipers()
+    this.getGroups()
   }
 
   // 渲染轮播图结构
   renderSwipers() {
-    return this.state.swipers.map((item) => (
+    return this.state.swipers.map(item => (
       <a
         key={item.id}
-        href="http://www.tobychung.com"
-        style={{ display: "inline-block", width: "100%", height: 212 }}
+        href="http://itcast.cn"
+        style={{
+          display: 'inline-block',
+          width: '100%',
+          height: 212
+        }}
       >
         <img
           src={`http://localhost:8080${item.imgSrc}`}
           alt=""
-          style={{ width: "100%", verticalAlign: "top" }}
-          onLoad={() => {
-            // fire window resize event to change height
-            window.dispatchEvent(new Event("resize"));
-            this.setState({ imgHeight: "auto" });
-          }}
+          style={{ width: '100%', verticalAlign: 'top' }}
         />
       </a>
-    ));
+    ))
   }
 
   // 渲染导航菜单
   renderNavs() {
-    return navs.map((item) => (
+    return navs.map(item => (
       <Flex.Item
         key={item.id}
         onClick={() => this.props.history.push(item.path)}
@@ -116,7 +112,7 @@ export default class Index extends React.Component {
         <img src={item.img} alt="" />
         <h2>{item.title}</h2>
       </Flex.Item>
-    ));
+    ))
   }
 
   render() {
@@ -124,12 +120,12 @@ export default class Index extends React.Component {
       <div className="index">
         {/* 轮播图 */}
         <div className="swiper">
-          {this.state.isSwipersLoaded ? (
-            <Carousel autoplay={true} infinite autoplayInterval={5000}>
+          {this.state.isSwiperLoaded ? (
+            <Carousel autoplay infinite autoplayInterval={5000}>
               {this.renderSwipers()}
             </Carousel>
           ) : (
-            ""
+            ''
           )}
         </div>
 
@@ -141,27 +137,36 @@ export default class Index extends React.Component {
           <h3 className="group-title">
             租房小组 <span className="more">更多</span>
           </h3>
+
+          {/* 宫格组件 */}
+          <Grid
+            data={this.state.groups}
+            columnNum={2}
+            square={false}
+            hasLine={false}
+            renderItem={item => (
+              <Flex className="group-item" justify="around" key={item.id}>
+                <div className="desc">
+                  <p className="title">{item.title}</p>
+                  <span className="info">{item.desc}</span>
+                </div>
+                <img src={`http://localhost:8080${item.imgSrc}`} alt="" />
+              </Flex>
+            )}
+          />
         </div>
 
-        <Grid
-          data={data}
-          columnNum={2}
-          square={false}
-          hasLine={false}
-          renderItem={() => (
-            <Flex className="group-item" justify="around">
-              <div className="desc">
-                <p className="title">家住回龙观</p>
-                <span className="info">归属的感觉</span>
-              </div>
-              <img
-                src="http://localhost:3000/static/media/group-1.263b84b0.png"
-                alt=""
-              />
-            </Flex>
-          )}
-        />
+        {/* <Flex className="group-item" justify="around">
+          <div className="desc">
+            <p className="title">家住回龙观</p>
+            <span className="info">归属的感觉</span>
+          </div>
+          <img
+            src="http://localhost:3000/static/media/group-1.263b84b0.png"
+            alt=""
+          />
+        </Flex> */}
       </div>
-    );
+    )
   }
 }
